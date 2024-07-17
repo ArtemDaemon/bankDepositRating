328076759a2c8e5737a185145e8ffa17458ceba3
ArtemDaemon <artem.daemon.official@gmail.com>
2024-07-17 19:08:31 +0700

    Добавил точный расчет для некапитализируемого счета

--- main.go
+++ main.go
@@ -5,10 +5,40 @@ import (
 	"fmt"
 )
 
+func getValidInt() int {
+	var i int
+	for {
+		_, err := fmt.Scanf("%d", &i)
+		if err != nil || i < 0 {
+			fmt.Println("Некорректный ввод! Введите положительное целое число")
+		} else {
+			return i
+		}
+	}
+
+}
+
 func main() {
 	bankDeposits := repos.GetBankDeposits()
+
+	fmt.Println("Введите начальную сумму")
+	initialSum := getValidInt()
+
+	fmt.Println("Введите сумму ежемесячного пополнения")
+	monthlyPayment := getValidInt()
+
+	fmt.Println("Введите срок инвестирования в годах")
+	investmentPeriod := getValidInt()
+
+	fmt.Printf("Итоговые вложения - %d\n", initialSum+monthlyPayment*investmentPeriod*12)
+
+	repos.MakeCalculations(bankDeposits, initialSum, monthlyPayment, investmentPeriod)
+	repos.SortByEndSum(bankDeposits)
 	for _, bankDeposit := range *bankDeposits {
-		fmt.Println(bankDeposit.Id, bankDeposit.BankName, bankDeposit.DepositName, bankDeposit.Rate,
-			bankDeposit.NumberOfMonths)
+		fmt.Printf("%s %s (срок (мес.) - %d, ставка - %f%%) - Выручка: %f, Итог: %f", bankDeposit.BankName,
+			bankDeposit.DepositName, bankDeposit.NumberOfMonths, bankDeposit.Rate, bankDeposit.TotalRevenue,
+			bankDeposit.EndSum)
+		fmt.Println()
 	}
+	// TODO: Добавить ежемесячную капитализацию
 }
--- repos/bankDepositRepository.go
+++ repos/bankDepositRepository.go
@@ -2,7 +2,18 @@ package repos
 
 import (
 	"bankDepositRating/database"
+	"math"
+	"sort"
 )
+
+type Calculation struct {
+	StartMonth int
+	EndMonth   int
+	StartSum   float64
+	Revenue    float64
+	Savings    float64
+	EndSum     float64
+}
 
 type BankDeposit struct {
 	Id             int
@@ -10,6 +21,10 @@ type BankDeposit struct {
 	DepositName    string
 	Rate           float64
 	NumberOfMonths int
+	Capitalization bool
+	Calculations   []Calculation
+	EndSum         float64
+	TotalRevenue   float64
 }
 
 func GetBankDeposits() *[]BankDeposit {
@@ -19,11 +34,65 @@ func GetBankDeposits() *[]BankDeposit {
 	for rows.Next() {
 		var bankDeposit BankDeposit
 		err := rows.Scan(&bankDeposit.Id, &bankDeposit.BankName, &bankDeposit.DepositName, &bankDeposit.Rate,
-			&bankDeposit.NumberOfMonths)
+			&bankDeposit.NumberOfMonths, &bankDeposit.Capitalization)
 		if err != nil {
 			panic(err)
 		}
 		bankDeposits = append(bankDeposits, bankDeposit)
 	}
 	return &bankDeposits
+}
+
+func MakeCalculations(bankDeposits *[]BankDeposit, initialSum int, monthlyPayment int, investmentPeriod int) {
+	investmentPeriodInMonths := investmentPeriod * 12
+	for i := range *bankDeposits {
+		bankDeposit := &(*bankDeposits)[i]
+		if bankDeposit.Calculations == nil {
+			bankDeposit.Calculations = make([]Calculation, 0)
+		}
+		startMonth := 0
+		startSum := float64(initialSum)
+		var endSum float64
+		for startMonth+bankDeposit.NumberOfMonths <= investmentPeriodInMonths {
+			var revenue float64
+			endMonth := startMonth + bankDeposit.NumberOfMonths
+			savings := float64(bankDeposit.NumberOfMonths * monthlyPayment)
+			depositLength := float64(31 * bankDeposit.NumberOfMonths)
+
+			newCalculation := Calculation{
+				StartMonth: startMonth,
+				EndMonth:   endMonth,
+				StartSum:   startSum,
+				Savings:    savings,
+			}
+
+			if bankDeposit.Capitalization {
+				revenue = startSum * math.Pow(1+bankDeposit.Rate/100/366, depositLength)
+			} else {
+				revenue = (startSum * bankDeposit.Rate * (depositLength / 366)) / 100
+			}
+			endSum = startSum + savings + revenue
+
+			newCalculation.Revenue = revenue
+			newCalculation.EndSum = endSum
+			bankDeposit.Calculations = append(bankDeposit.Calculations, newCalculation)
+
+			startMonth = endMonth
+			startSum = endSum
+
+			bankDeposit.TotalRevenue += revenue
+		}
+		bankDeposit.EndSum = endSum
+
+		if startMonth != investmentPeriodInMonths {
+			diff := investmentPeriodInMonths - startMonth
+			bankDeposit.EndSum += float64(diff * monthlyPayment)
+		}
+	}
+}
+
+func SortByEndSum(bankDeposits *[]BankDeposit) {
+	sort.Slice(*bankDeposits, func(i, j int) bool {
+		return (*bankDeposits)[i].EndSum > (*bankDeposits)[j].EndSum
+	})
 }
